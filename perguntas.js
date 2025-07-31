af883ff69f731924bac7f2889c1851c6ef98af52
yasuo275 <bryan.cesario.santos@escola.pr.gov.br>
2025-07-31 09:02:45 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+
+const perguntas = [
+{
+categoria: "[Categoria do Aluno]",
+pergunta: "[Pergunta 1 do Aluno]",
+resposta: "[Resposta 1 do Aluno]"
+},
+{
+categoria: "[Categoria do Aluno]",
+pergunta: "[Pergunta 2 do Aluno]",
+resposta: "[Resposta 2 do Aluno]"
+},
+{
+categoria: "[Categoria do Aluno]",
+pergunta: "[Pergunta 3 do Aluno]",
+resposta: "[Resposta 3 do Aluno]"
+},
+{
+categoria: "[Categoria do Aluno]",
+pergunta: "[Pergunta 4 do Aluno]",
+resposta: "[Resposta 4 do Aluno]"
+},
+{
+categoria: "[Categoria do Aluno]",
+pergunta: "[Pergunta 5 do Aluno]",
+resposta: "[Resposta 5 do Aluno]"
+}
+];
