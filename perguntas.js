728fe5a4df2842e1636f1c884c091858bf1dfb8d
yasuo275 <bryan.cesario.santos@escola.pr.gov.br>
2025-07-31 10:40:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,27 +1,27 @@
 const perguntas = [
 {
-categoria: "[Matemática]",
-pergunta: "[Como resolver uma equação do segundo grau]",
-resposta: "[Primeiro, identificamos os coeficientes, depois aplicamos a fórmula de Bhaskara.]"
+categoria: "Matemática",
+pergunta: "Como resolver uma equação do segundo grau",
+resposta: "Primeiro, identificamos os coeficientes, depois aplicamos a fórmula de Bhaskara."
 },
 {
-categoria: "[Física]",
-pergunta: "[Qual é a fórmula da força]",
-resposta: "[A fórmula da força é F = m * a, onde 'm' é a massa e 'a' é a aceleração.]"
+categoria: "Física",
+pergunta: "Qual é a fórmula da força",
+resposta: "A fórmula da força é F = m * a, onde 'm' é a massa e 'a' é a aceleração."
 },
 {
-categoria: "[Química]",
-pergunta: "[O que é uma reação endotérmica]",
-resposta: "[É uma reação que absorve calor do ambiente durante o processo.]"
+categoria: "Química",
+pergunta: "O que é uma reação endotérmica",
+resposta: "É uma reação que absorve calor do ambiente durante o processo."
 },
 {
-categoria: "[História]",
-pergunta: "[Quem foi o primeiro presidente do Brasil?]",
-resposta: "[O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca.]"
+categoria: "História",
+pergunta: "Quem foi o primeiro presidente do Brasil?",
+resposta: "O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca."
 },
 {
-categoria: "[Geografia]",
-pergunta: "[Quais são os continentes da Terra?]",
-resposta: "[Quais são os continentes da Terra?",]"
+categoria: "Geografia",
+pergunta: "Quais são os continentes da Terra",
+resposta: "Quais são os continentes da Terra?"
 }
 ];
